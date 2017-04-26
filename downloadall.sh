2a37f360e696602c89c140ccb827ace2afb2fb42
amazingfate <liujianfeng1994@gmail.com>
2017-04-26 09:29:05 +0800

    add debug to downloadall
    Change-Id: I955505bc115cd4cfe2af0fc8dd49036b3d4c378f

--- downloadall.sh
+++ downloadall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 while read project
 do
+	echo "start download $project"
 	PROJECT=$project docker-compose run --rm sync-transifex DownloadPo|| echo "$project download failed"
 done < project.list
