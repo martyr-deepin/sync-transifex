0ec80c8eccf7f146857aa1e16d201cce51dd3b62
amazingfate <liujianfeng1994@gmail.com>
2017-04-26 10:55:13 +0800

    fix downloadall.sh
    Change-Id: Id1ef4adf05b088cddabe38b8b36ff63cc1a8563f

--- source/downloadall.sh
+++ source/downloadall.sh
@@ -2,5 +2,5 @@
 while read project_name
 do
 	echo "start download $project_name"
-	PROJECT=$project_name sync_po.sh DownloadPo|| echo "$project_name download failed"
+	PROJECT=$project_name bash -x sync_po.sh DownloadPo|| echo "$project_name download failed"
 done < project.list
