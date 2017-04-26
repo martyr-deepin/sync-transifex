88e084281e1bbf1f7a726e17f0e1626c7cbefae8
amazingfate <liujianfeng1994@gmail.com>
2017-04-26 10:10:00 +0800

    fix downloadall.sh
    Change-Id: I06af8fc841ac30524d7ec4964a704d19962fcf62

--- downloadall.sh
+++ downloadall.sh
@@ -3,4 +3,6 @@ while read project_name
 do
 	echo "start download $project_name"
 	PROJECT=$project_name docker-compose run --rm sync-transifex DownloadPo|| echo "$project_name download failed"
+	docker stop sync-transifex||echo sync-transifex not running
+	docker rm sync-transifex|| echo sync-transifex is not a container name
 done < project.list
